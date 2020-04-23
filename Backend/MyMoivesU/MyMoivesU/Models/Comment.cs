938a36b2427c8fe9f43d496c96cf429320444f41
ghassan <gh09an@gmail.com>
2020-04-23 18:55:18 -0400

    created relationship for comment model

--- Backend/MyMoivesU/MyMoivesU/Models/Comment.cs
+++ Backend/MyMoivesU/MyMoivesU/Models/Comment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MyMoivesU.Models
@@ -9,5 +10,23 @@ namespace MyMoivesU.Models
     {  
         public int Id { get; set; }
         public string CommentText { get; set; }
+
+        [JsonIgnore]
+        public virtual User User { get; set; }
+        public int UserId { get; set; }
+
+        [JsonIgnore]
+        public virtual Movie Movie { get; set; }
+        public int MovieId { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(int id, string commentText)
+        {
+            Id = id;
+            CommentText = commentText;
+        }
     }
 }
