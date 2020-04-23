e2e505b59d5715942cd447fc93f3e79724a962e6
ghassan <gh09an@gmail.com>
2020-04-23 18:24:35 -0400

    created chat model

---
+++ Backend/MyMoivesU/MyMoivesU/Models/Chat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMoivesU.Models
+{
+    public class Chat
+    {  
+        public int Id { get; set; }
+        public string CommentText { get; set; }
+    }
+}
