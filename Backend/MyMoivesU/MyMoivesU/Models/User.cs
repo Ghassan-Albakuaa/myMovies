5ef70a08bed43adb43e8c01a424cf8c5c0494a73
ghassan <gh09an@gmail.com>
2020-04-23 18:36:40 -0400

    created relationship for user model

---
+++ Backend/MyMoivesU/MyMoivesU/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MyMoivesU.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public string Password { get; set; }
+        public string Image { get; set; }
+
+
+        [JsonIgnore]
+        public virtual IList<Watchlist> Watchlists { get; set; }
+        [JsonIgnore]
+        public virtual IList<Comment> Comments { get; set; }
+
+
+        public User()
+        {
+        }
+
+        public User(int id, string name, string password, string group, string image)
+        {
+            Id = id;
+            Name = name;
+            Group = group;
+            Password = password;
+            Image = image;
+        }
+    }
+}
