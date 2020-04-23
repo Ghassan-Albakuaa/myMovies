b11b48460ad8ac07836965e6b2ddb4a687f15fcd
ghassan <gh09an@gmail.com>
2020-04-23 18:50:50 -0400

    created relationship for watchlist model

--- Backend/MyMoivesU/MyMoivesU/Models/Watchlist.cs
+++ Backend/MyMoivesU/MyMoivesU/Models/Watchlist.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MyMoivesU.Models
@@ -11,5 +12,25 @@ namespace MyMoivesU.Models
         public string Status { get; set; }
         public string Review { get; set; }
         public string Rating { get; set; }
+
+
+        [JsonIgnore]
+        public virtual User User { get; set; }
+        public int UserId { get; set; }
+
+        [JsonIgnore]
+        public virtual Movie Movie { get; set; }
+        public int MovieId { get; set; }
+        public Watchlist()
+        {
+        }
+
+        public Watchlist(int id, string review, string rating)
+        {
+            Id = id;
+            Review = review;
+            Rating = rating;
+        }
+
     }
 }
