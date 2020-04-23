70aa9653638582942eaa487aece23ebe14c05a55
ghassan <gh09an@gmail.com>
2020-04-23 19:45:10 -0400

    added ef packages and created moviecontext

---
+++ Backend/MyMoivesU/MyMoivesU/MovieContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMoivesU.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMoivesU
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Watchlist> Watchlists { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=MovieUDb;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
