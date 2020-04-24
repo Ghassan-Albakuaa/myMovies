fecbe5c90b6c1207134544ddf7aa32706acdd562
ghassan <gh09an@gmail.com>
2020-04-23 20:49:33 -0400

    added IRepository

---
+++ Backend/MyMoivesU/MyMoivesU/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMoivesU.Repository
+{
+   public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetByUserId(int id);
+        IEnumerable<T> GetByTvShowId(int id);
+        T GetById(int id);
+        void Create(T obj);
+        void Update(T obj);
+        void Delete(T obj);
+    }
+    
+}
