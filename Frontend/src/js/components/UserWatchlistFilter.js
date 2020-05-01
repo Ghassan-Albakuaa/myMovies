edfc0da5da4d4e85cc5a0f5761c5aeb042a2ce77
ghassan <gh09an@gmail.com>
2020-05-01 10:25:59 -0400

    edit UserWatchlistFilter

--- Frontend/src/js/components/UserWatchlistFilter.js
+++ Frontend/src/js/components/UserWatchlistFilter.js
@@ -1,11 +1,11 @@
 export default function UserWatchlistFilter(watchlist){
     return `
       ${watchlist.map(element => {
-         
+        if(element.status == "Watched" ){
               const watchedShow = document.createElement("p");
               watchedShow.classList.add("watchlist__watched_movie");
               document.querySelector(".watchlist__watched_movies").appendChild(watchedShow);
-              watchedShow.innerHTML +=  `<h5 class="watched_movie__title">${element.movie.title} <p class="p_title-hover-btn">>></p></h5>
+              watchedShow.innerHTML +=  `<h5 class="watched_movie__title">${element.movie.title} <p class="p_title-hover-btn"></p></h5>
               <input class="watch__id"  type="hidden" value="${element.id}">
               <input class="watch__Userid"  type="hidden" value="${element.user.id}">`
             + ` <img class="movie__image" src="./img/movies/${element.movie.image}" alt="${element.movie.image}"></img>`
@@ -14,7 +14,35 @@ export default function UserWatchlistFilter(watchlist){
             + ` <button class='edit-watchList__submit'>Edit</button>` 
             + ` <button class='addReview-watchList__submit'>Add Review </button>`
             + ` <button class='deleteReview-watchList__submit'>Delete</button>` ;
-          
+        }
+
+        if(element.status == "Watching" ){
+          const watchedShow = document.createElement("p");
+          watchedShow.classList.add("watchlist__watching_movie");
+          document.querySelector(".watchlist__watching_movies").appendChild(watchedShow);
+          watchedShow.innerHTML +=  `<h5 class="watched_movie__title">${element.movie.title} <p class="p_title-hover-btn"></p></h5>
+          <input class="watch__id"  type="hidden" value="${element.id}">
+          <input class="watch__Userid"  type="hidden" value="${element.user.id}">`
+        + ` <img class="movie__image" src="./img/movies/${element.movie.image}" alt="${element.movie.image}"></img>`
+        + ` </br>`
+        + ` <input class="watch__tvShowId" type="hidden" value="${element.movieId}">`
+        + ` <button class='edit-watchList__submit'>Edit</button>`        
+        + ` <button class='deleteReview-watchList__submit'>Delete</button>` ;
+    }
+
+    if(element.status == "ToBeWatched" ){
+      const watchedShow = document.createElement("p");
+      watchedShow.classList.add("watchlist__tobewatched_movie");
+      document.querySelector(".watchlist__tobewatched_movies").appendChild(watchedShow);
+      watchedShow.innerHTML +=  `<h5 class="watched_movie__title">${element.movie.title} <p class="p_title-hover-btn"></p></h5>
+      <input class="watch__id"  type="hidden" value="${element.id}">
+      <input class="watch__Userid"  type="hidden" value="${element.user.id}">`
+    + ` <img class="movie__image" src="./img/movies/${element.movie.image}" alt="${element.movie.image}"></img>`
+    + ` </br>`
+    + ` <input class="watch__tvShowId" type="hidden" value="${element.movieId}">`
+    + ` <button class='edit-watchList__submit'>Edit</button>`        
+    + ` <button class='deleteReview-watchList__submit'>Delete</button>` ;
+}
             }).join("")}
             `
         }
