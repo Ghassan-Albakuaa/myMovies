b99a90dacc7b17b586c2472f187a1cf42ca97dfb
ghassan <gh09an@gmail.com>
2020-05-01 18:37:51 -0400

    filter WatchlistGrid

--- Frontend/src/js/components/WatchlistGrid.js
+++ Frontend/src/js/components/WatchlistGrid.js
@@ -1,6 +1,6 @@
 export default function WatchlistGrid(){
     
-   // if(localStorage.LoginhUserId == localStorage.vistorId)
+    if(localStorage.LoginhUserId == localStorage.vistorId)
     return ` 
     <section class="watchlist__add_movie">
         <button class="watchlist__add_movie_button">Add a movie to your list</button>
@@ -20,5 +20,21 @@ export default function WatchlistGrid(){
         </div>
     </div>
     `
-    
+    else
+    return ` 
+    <div class="watchlist__grid_items_container">
+        <div class="watchlist__watching_movies">
+            <h3>Watching</h3>
+            <hr>
+        </div>
+        <div class="watchlist__tobewatched_movies">
+            <h3>To Be Watched</h3>
+            <hr>
+        </div>
+        <div class="watchlist__watched_movies">
+            <h3>Watched</h3>
+            <hr>
+        </div>
+    </div>
+    `
 }
