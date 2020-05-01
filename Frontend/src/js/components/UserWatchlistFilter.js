8e089a7ed9bb70b642a9aa0bf49e2ccbc8da36d2
ghassan <gh09an@gmail.com>
2020-05-01 18:30:54 -0400

    filter UserWatchlistFilter

--- Frontend/src/js/components/UserWatchlistFilter.js
+++ Frontend/src/js/components/UserWatchlistFilter.js
@@ -1,7 +1,7 @@
 export default function UserWatchlistFilter(watchlist){
     return `
       ${watchlist.map(element => {
-        if(element.status == "Watched" ){
+        if(element.status == "Watched" && localStorage.LoginhUserId == element.user.id){
               const watchedShow = document.createElement("p");
               watchedShow.classList.add("watchlist__watched_movie");
               document.querySelector(".watchlist__watched_movies").appendChild(watchedShow);
@@ -16,7 +16,7 @@ export default function UserWatchlistFilter(watchlist){
             + ` <button class='deleteReview-watchList__submit'>Delete</button>` ;
         }
 
-        if(element.status == "Watching" ){
+        if(element.status == "Watching" && localStorage.LoginhUserId == element.user.id){
           const watchedShow = document.createElement("p");
           watchedShow.classList.add("watchlist__watching_movie");
           document.querySelector(".watchlist__watching_movies").appendChild(watchedShow);
@@ -30,7 +30,7 @@ export default function UserWatchlistFilter(watchlist){
         + ` <button class='deleteReview-watchList__submit'>Delete</button>` ;
     }
 
-    if(element.status == "ToBeWatched" ){
+    if(element.status == "ToBeWatched" && localStorage.LoginhUserId == element.user.id){
       const watchedShow = document.createElement("p");
       watchedShow.classList.add("watchlist__tobewatched_movie");
       document.querySelector(".watchlist__tobewatched_movies").appendChild(watchedShow);
@@ -42,6 +42,45 @@ export default function UserWatchlistFilter(watchlist){
     + ` <input class="watch__movie" type="hidden" value="${element.movieId}">`
     + ` <button class='edit-watchList__submit'>Edit</button>`        
     + ` <button class='deleteReview-watchList__submit'>Delete</button>` ;
+}
+
+if(element.status == "Watched" && localStorage.LoginhUserId != element.user.id){
+  const watchedShow = document.createElement("p");
+  watchedShow.classList.add("watchlist__watched_movie");
+  document.querySelector(".watchlist__watched_movies").appendChild(watchedShow);
+  watchedShow.innerHTML +=  `<h5 class="watched_movie__title">${element.movie.title} <p class="p_title-hover-btn"></p></h5>
+  <input class="watch__id"  type="hidden" value="${element.id}">
+  <input class="watch__Userid"  type="hidden" value="${element.user.id}">`
++ ` <img class="movie__image" src="./img/movies/${element.movie.image}" alt="${element.movie.image}"></img>`
++ ` </br>`
++ ` <input class="watch__movieId" type="hidden" value="${element.movieId}">`
+ ;
+}
+
+if(element.status == "Watching" && localStorage.LoginhUserId != element.user.id){
+const watchedShow = document.createElement("p");
+watchedShow.classList.add("watchlist__watching_movie");
+document.querySelector(".watchlist__watching_movies").appendChild(watchedShow);
+watchedShow.innerHTML +=  `<h5 class="watched_movie__title">${element.movie.title} <p class="p_title-hover-btn"></p></h5>
+<input class="watch__id"  type="hidden" value="${element.id}">
+<input class="watch__Userid"  type="hidden" value="${element.user.id}">`
++ ` <img class="movie__image" src="./img/movies/${element.movie.image}" alt="${element.movie.image}"></img>`
++ ` </br>`
++ ` <input class="watch__movie" type="hidden" value="${element.movieId}">`
+ ;
+}
+
+if(element.status == "ToBeWatched" && localStorage.LoginhUserId == element.user.id){
+const watchedShow = document.createElement("p");
+watchedShow.classList.add("watchlist__tobewatched_movie");
+document.querySelector(".watchlist__tobewatched_movies").appendChild(watchedShow);
+watchedShow.innerHTML +=  `<h5 class="watched_movie__title">${element.movie.title} <p class="p_title-hover-btn"></p></h5>
+<input class="watch__id"  type="hidden" value="${element.id}">
+<input class="watch__Userid"  type="hidden" value="${element.user.id}">`
++ ` <img class="movie__image" src="./img/movies/${element.movie.image}" alt="${element.movie.image}"></img>`
++ ` </br>`
++ ` <input class="watch__movie" type="hidden" value="${element.movieId}">`
+ ;
 }
             }).join("")}
             `
